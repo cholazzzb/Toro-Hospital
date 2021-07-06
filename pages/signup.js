c3b208de77ce770936b40d88fc231eecd0ea7582
cholazzzb <nicsphehehe@gmail.com>
2021-07-06 07:27:35 +0700

    Add login signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,16 +1,19 @@
 import { TextField } from "@material-ui/core";
 import Head from "next/head";
 
-export default function login() {
+export default function signup() {
   return (
     <div className="background colorText">
       <Head>
-        <title>Login</title>
+        <title>Signup</title>
       </Head>
       <div className="flex w-full items-center justify-center">
         <div className="color1 flex flex-col p-4 text-center rounded-lg">
           Welcome to Toro Hospital
           <form className="flex flex-col">
+            <div className="mt-4">
+              <TextField label="Username" variant="outlined" />
+            </div>
             <div className="mt-4">
               <TextField label="Email" variant="outlined" />
             </div>
@@ -18,11 +21,11 @@ export default function login() {
               <TextField label="Password" variant="outlined" />
             </div>
             <button className="bg-purple-700 hover:bg-purple-800 rounded-xl p-4 my-2">
-              Login
+              Sign up
             </button>
-            New on our platform?
-            <a className="cursor-pointer text-purple-700" href="/signup">
-              Create an account
+            Already have an account?
+            <a className="cursor-pointer text-purple-700" href="/login">
+              Log in instead
             </a>
           </form>
         </div>
